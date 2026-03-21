47e6aac4d091629c4b91d3da451949c1b3be882f
YvanLouise <yvanlouiseks@gmail.com>
2026-03-21 17:00:31 +0800

    Up21a下e 31i下e 2026-03-21 17:00:31

--- public-site/src/main.tsx
+++ public-site/src/main.tsx
@@ -9,7 +9,7 @@ import "@shared/styles/main.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <MotionConfig reducedMotion="user">
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
       </BrowserRouter>
     </MotionConfig>
